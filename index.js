90456a0088ba1d4ee9721f93e1f552b18e9e1bbe
Eric DeSimone <eric.desimone@cortland.edu>
2018-08-24 16:04:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,3 +20,9 @@ function getToken() {
   //back to '' before committing so all tests pass
   return ''
 }
+
+// fetch('https://api.github.com/user/repos', {
+//   headers: {
+//     Authorization: `token ${token}`
+//   }
+// }).then(res => res.json()).then(json => console.log(json));
